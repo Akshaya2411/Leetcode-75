6e4e2751f5d698a4a121ee198f7bc9b04e1c37da
Akshaya2411 <65708503+Akshaya2411@users.noreply.github.com>
2024-02-21 21:04:33 +0530

    Update ProductOfArrayExceptSelf.py

--- ProductOfArrayExceptSelf.py
+++ ProductOfArrayExceptSelf.py
@@ -2,8 +2,12 @@ from numpy import prod
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         res=[]
+        m=prod(nums)
         for i in range(0,len(nums)):
-            c=nums[:i]+nums[i+1:]
-            res.append(prod(c))
+            if(nums[i]==0):
+                c=nums[:i]+nums[i+1:]
+                res.append(prod(c))
+            else:
+                res.append(m//nums[i])
         return res
         

3cdc6989e0dc2c734f636bd9616edcaa7c71dc5d
Akshaya2411 <65708503+Akshaya2411@users.noreply.github.com>
2024-03-17 17:57:04 +0530

    Update DetermineIfTwoStringsAreClose.py

--- DetermineIfTwoStringsAreClose.py
+++ DetermineIfTwoStringsAreClose.py
@@ -7,16 +7,18 @@ class Solution:
           b=[]
           c=list(set(list(word1)))
           d=list(set(list(word2)))
-          print(c)
-          print(d)
-          for i in c:
+          for i,j in zip(c,d):
             b.append(word2.count(i))
-          for i in d:
-            a.append(word1.count(i))
+            a.append(word1.count(j))
           if(sorted(a)==sorted(b) and sorted(c)==sorted(d)):
             return True
           else:
             return False
+        
+
+
+        
+        
         
 
 
